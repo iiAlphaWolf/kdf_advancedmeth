92a2895eb3b11a8aa1351631a30d86eeb7445a7b
iiAlphaWolf <30497126+iiAlphaWolf@users.noreply.github.com>
2020-08-13 00:42:19 +0200

    Version b1.0.0

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config                            = {}
+Config.DrawDistance               = 5.0
+
+Config.Zones = {
+  methlab_maken = {
+    Pos   = { x = 472.05, y = -1310.56, z = 29.22 },
+    Size  = { x = 0.5, y = 0.5, z = 0.5 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 2,
+  },
+  meth_process = {
+    Pos   = { x = 2432.3, y = 4971.74, z = 42.35 },
+    Size  = { x = 0.5, y = 0.5, z = 0.5 },
+    Color = { r = 0, g = 255, b = 0 },  
+    Type  = 2,
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "bodacious"
+game "gta5"
+
+author "KleurenDoof (Ralph Stoop)"
+description "Advanced (Breaking Bad) meth script"
+version "1.0.0"
+ui_page "h.html"
+
+client_script {
+	"client.lua",
+	"config.lua"
+}
+server_script {
+	"server.lua",
+	"config.lua"
+}
+
+files {
+    "h.html"
+}
